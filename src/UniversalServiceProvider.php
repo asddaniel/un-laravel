ccc949e4d7ce6a123a173ed7964dea01bf240e28
Daniel Assani <devasddaniel@gmail.com>
2023-02-02 09:10:47 +0200

    initial commit

---
+++ src/UniversalServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Asddaniel\UniversalLaravel;
+class UniversalServiceProvider extends PackageServiceProvider{
+
+    public function configurePackage(Package $package): void
+    {
+        $package
+        ->name('laravel-universal')
+        ->hasMigrations(["2023_01_31_072001_create_donnees_table", 
+        "2023_01_31_072119_create_relations_table", 
+        "2023_01_31_072221_create_colonnes_table", 
+        "2023_01_31_072352_create_enregistrements_table"])
+        ->runsMigrations();
+        ;
+    }
+}
